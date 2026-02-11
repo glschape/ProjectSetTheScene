83fbcadc4b6303acd8e22899ac7fce2979452a45
glschape <glschape@mtu.edu>
2026-02-11 11:38:12 -0500

    Update test.java
    Test program now prints out two lines.

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
   public static void main(String[] args) {
       System.out.println("I swear, I'm not a robot!");
-
+      System.out.println("Jacob is so nice and helpful :D");
   }
 }
